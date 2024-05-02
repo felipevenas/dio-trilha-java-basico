1c412680fe81c96f3bf2551419c11b74e2ba7334
felipevenas <dev.felipevenas@gmail.com>
2024-05-01 22:09:53 -0300

    Segundo-Commit

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String[] args) {
+        
+        final int ANO_NASCIMENTO = 2005;
+        int idade;
+        String nome = "Felipe";
+        double salario = 1400;
+        long cep = 41650195;
+        idade = 2024 - ANO_NASCIMENTO;
+
+            System.out.println("Hello World!");
+            System.out.println("Nome: " + nome + ".");
+            System.out.println("Idade: " + idade + ".");
+            System.out.println("Salário:: " + salario + ".");
+            System.out.println("CEP: " + cep + ".");
     }
 }
