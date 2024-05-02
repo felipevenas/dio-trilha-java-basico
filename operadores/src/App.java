820d50765ee3465fa2070c41bb5bdc80bafaee3b
felipevenas <dev.felipevenas@gmail.com>
2024-05-01 22:23:44 -0300

    Exemplos-operadores

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -1,6 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        // Operadores de atribuição: | =
+        String nome = "nome";
+        int idade = 19;
+        double peso = 81.5;
+        char sexo = "M";
+        boolean doadorOrgao = true;
+        Date dataNascimento = new Date();
 
-        System.out.println("Hello, World!");
+        // Operadores aritméticos: | + - * / %
+        double soma = 10.4 + 12.6;
+        int subtracao = 100 - 50;
+        int modulo = 18 % 3; // Armazena o resto da divisão.
+        double resultado = (10 * 5) + (50 / 5);
+        
     }
 }
