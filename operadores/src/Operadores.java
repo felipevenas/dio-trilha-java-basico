ef46a4baccb287097c377f1b183f24d86dadde22
felipevenas <dev.felipevenas@gmail.com>
2024-05-02 22:29:07 -0300

    Novo nome de Class

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,19 +1,25 @@
-public class App {
+public class Operadores {
     public static void main(String[] args) throws Exception {
         
         // Operador de atribuição: | =
         String nome = "nome";
         int idade = 19;
         double peso = 81.5;
-        char sexo = "M";
+        char sexo = 'M';
         boolean doadorOrgao = true;
-        Date dataNascimento = new Date();
 
         // Operadores aritméticos: | + - * / %
         double soma = 10.4 + 12.6;
         int subtracao = 100 - 50;
         int modulo = 18 % 3; // Armazena o resto da divisão.
         double resultado = (10 * 5) + (50 / 5);
+        
+        // Operadores unários: | (+) (-) (++) (--) (!)
+        int numero = 5; 
+            System.out.println(-- numero); 
+            System.out.println(++ numero);
+            System.out.println(- numero);
+            System.out.println(+ numero);
 
         
     }
