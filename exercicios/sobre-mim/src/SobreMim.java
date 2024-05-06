9009267954b8086d7b203c9f95e9abce4442b86e
felipevenas <dev.felipevenas@gmail.com>
2024-05-06 16:58:24 -0300

    Criando-um-Scanner

---
+++ exercicios/sobre-mim/src/SobreMim.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class SobreMim {
+    public static void main(String[] args) throws Exception {
+
+        String nome, sobrenome;
+        int idade;
+        double altura;
+
+        // Criando o objeto "scanner":
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+            nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+            sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+            idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+            altura = scanner.nextDouble();
+        
+        System.out.println("\nNome completo: " + nome + " " + sobrenome + "." );
+        System.out.println("Idade: " + idade + "." );
+        System.out.println("Altura computada: " + altura + "cm." );
+
+        scanner.close();
+
+        
+        
+        
+        
+    }
+}
