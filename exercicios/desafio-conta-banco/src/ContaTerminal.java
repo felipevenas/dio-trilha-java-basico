dac4c41ed67e2cb6423cfc417966d9a6c93487fa
felipevenas <dev.felipevenas@gmail.com>
2024-05-06 21:38:13 -0300

    Desafio-banco

---
+++ exercicios/desafio-conta-banco/src/ContaTerminal.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        String nome, sobrenome;
+        int idConta, idAgencia, idAgenciaDigito, feedback; 
+        double saldo = 3000;
+
+        System.out.println("Por favor, digite seu primeiro nome: ");
+            nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+            sobrenome = scanner.nextLine();
+
+        String nomeCompleto = (nome + " " + sobrenome);
+
+        System.out.println("Agora, digite o número de sua conta | XXXX: ");   
+            idConta = scanner.nextInt();
+        
+        System.out.println("Digite os três números da sua agência | XXX: ");
+            idAgencia = scanner.nextInt();
+
+        System.out.println("Dígito | " + idAgencia + "-?: ");
+            idAgenciaDigito = scanner.nextInt();
+
+        System.out.println();
+            
+        System.out.println("Olá " + nome + ", me chamo Eko, Assistente Virtual do uBank.");
+        System.out.println("Gostaria de lhe dar boas-vindas para sua nova experiência financeira inovadora!");
+
+        System.out.println();
+
+        System.out.println("Esses são os seus dados da sua nova conta digital: ");
+        System.out.println("Nome: " + nomeCompleto + ".");
+        System.out.println("Conta: " + idConta + " | Agência: " + idAgencia + "-" + idAgenciaDigito + ".");
+        System.out.println("Saldo disponível: R$" + saldo + ".");
+
+        System.out.println();
+
+        System.out.println("Avalie o nosso aplicativo em uma escala de 1-5: ");
+            feedback = scanner.nextInt();
+
+        scanner.close();
+    }
+}
