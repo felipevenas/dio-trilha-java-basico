4fa873844e61ada7b8ced822b2f24c332707d195
felipevenas <dev.felipevenas@gmail.com>
2024-05-07 21:35:44 -0300

    Atualização na pasta de Estrutura Condicional

--- estruturas-condicionais/sistema-escola/src/SistemaEscola.java
+++ estruturas-condicionais/sistema-escola/src/SistemaEscola.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Locale;
 
-public class CaixaEletronico {
+public class SistemaEscola {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
