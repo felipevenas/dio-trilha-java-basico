c44ab5eac4f759154c2fc76a02edec24f0358648
felipevenas <dev.felipevenas@gmail.com>
2024-05-10 00:32:26 -0300

    Sistema-medida adicionado.

---
+++ exercicios/sistema-medida/src/SistemaMedida.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class SistemaMedida {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        String sigla = " ";
+
+        System.out.println("Escolha seu tamanho:");
+        System.out.println("P | M | G | GG | XG");
+            sigla = scanner.nextLine();
+
+        System.out.println();
+        
+        switch(sigla) {
+            case "P":{
+                System.out.println("Tamanho escolhido: Pequeno.");
+                break;
+            }
+            case "M":{
+                System.out.println("Tamanho escolhido: Médio.");
+                break;
+            }
+            case "G":{
+                System.out.println("Tamanho escolhido: Grande.");
+                break;
+            }
+            case "GG":{
+                System.out.println("Tamanho escolhido: Muito grande.");
+                break;
+            }
+            case "XG":{
+                System.out.println("Tamanho escolhido: Extra grande.");
+                break;
+            }
+            default: {
+                System.out.println("Tamanho indefinido.");
+            }
+        }
+    }L
+}
