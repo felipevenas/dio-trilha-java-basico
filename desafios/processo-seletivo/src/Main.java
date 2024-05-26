4fcd1d637a7b62f4ce2ff22d812f3d6b63fee67b
felipevenas <dev.felipevenas@gmail.com>
2024-05-26 12:25:58 -0300

    Atualização nas funções para o desafio de Sistema de Processo Seletivo da DIO.

--- desafios/processo-seletivo/src/Main.java
+++ desafios/processo-seletivo/src/Main.java
@@ -1,9 +1,12 @@
+import inscrição.ProcessoSeletivo;
 import java.util.Scanner;
 import java.util.Locale;
 
+import static inscrição.ProcessoSeletivo.*; // Importando todas as classes estáticas desse arquivo com o '*'.
+
+
 public class Main {
     public static void main(String[] args) {
-
 
 
     }
--- desafios/processo-seletivo/src/inscrição/ProcessoSeletivo.java
+++ desafios/processo-seletivo/src/inscrição/ProcessoSeletivo.java
@@ -1,17 +1,28 @@
 package inscrição;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String [] args) {
 
-        System.out.println("====== Processo Seletivo ======");
+        imprimirCandidatos();
 
         selecaoCandidatos();
 
-        /* analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0); */
+        String [] candidatos = {"Felipe", "Elthon", "Breno", "Alisson", "Costa", "Levi", "Rayan", "Luisa", "Amanda", "Kaique", "Nicolas"};
+        for (String candidato: candidatos) {
+            System.out.println("Classificação do candidato, " + candidato + ":");
+            analisarCandidato(salarioDesejado());
+            System.out.println();
+        }
+
+        for (String candidato: candidatos) {
+            contatarCandidatos(candidato);
+        }
+
+        selecaoCandidatos();
+
     }
 
     static void analisarCandidato(double salarioDesejado) {
@@ -52,9 +63,44 @@ public class ProcessoSeletivo {
         }
     }
 
-    static double salarioDesejado() { // Função para gerar valores aleatórios. | Simulação.
+    static void imprimirCandidatos() {
+        String [] candidatos = {"Felipe", "Elthon", "Breno", "Alisson", "Costa", "Levi", "Rayan", "Luisa", "Amanda", "Kaique", "Nicolas"};
+
+        System.out.println("Imprimindo a lista de candidatos: ");
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de número " + (indice + 1) + " é o " + candidatos[indice]);
+        }
+    }
+
+    static void contatarCandidatos(String candidato) {
+        String [] candidatos = {"Felipe", "Elthon", "Breno", "Alisson", "Costa", "Levi", "Rayan", "Luisa", "Amanda", "Kaique", "Nicolas"};
+
+            int tentativasRealizadas = 1;
+            boolean continuarTentando = true;
+            boolean atendeu = false;
+
+            do {
+                atendeu = atender();
+                continuarTentando = !atendeu;
+                if (continuarTentando)
+                    tentativasRealizadas++;
+                else
+                   System.out.println("Contato realizado com sucesso!");
+            } while (continuarTentando && tentativasRealizadas < 3);
+                if (atendeu)
+                    System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa.");
+                else
+                    System.out.println("Não conseguimos contato com " + candidato + "," + " número máximo de tentativas excedido.");
+
+            System.out.println();
+
+    }
+
+    static double salarioDesejado() { // Função para gerar valores aleatórios. | MÉTODO AUXILIAR.
         return ThreadLocalRandom.current().nextDouble(1500, 2200);
     }
 
-
+    static boolean atender() {  // Função booleana para gerar valores aleatórios. | MÉTODO AUXILIAR.
+        return new Random().nextInt(3)==1; // Cria uma estrutura randomica; Determina que o valor verdadeiro está entre 1 e 3; '==1' = Valor verdadeiro.
+    }
 }
